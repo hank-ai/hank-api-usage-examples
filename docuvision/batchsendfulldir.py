081483800a131dd178bb239b61ec7bf61ecf6680
Jack Neil <25892470+jackneil@users.noreply.github.com>
2022-02-05 10:00:40 -0500

    initial release. fully working

--- docuvision/batchsendfulldir.py
+++ docuvision/batchsendfulldir.py
@@ -5,46 +5,86 @@
 # 
 # Hank.ai DocuVision API Sample Code
 #
-# Batch send all files of given types in the given directory 
+# Batch send all files of given types in the given directory and get results
 # 
 # Prerequisites:
 # - Set ENV variables on the machine running this script. 
 #   - Set 'DOCUVISION_API_TOKEN' to the token given to you by hank.ai during signup
 #   - Set 'DOCUVISION_API_ADDRESS' to the url given to you by hank.ai during signup
 # - Python 3.7.7 or greater installed
-# - Python packages pandas and numpy. Install via 'pip install pandas' and 'pip install numpy'
-# Args: 
-# - types: pdf, png, or jpg. may include up to all 3 seperated by a comma. default=pdf,jpg,png
-# - dir: full path to a directory on the current machine you'd like to post to DocuVision API. default=current directory
-#
+# Args: (command line args. ex: python batchsendfulldir.py --types pdf --dir c:/test/ --loglevel DEBUG)
+# - --types: pdf, png, or jpg. may include up to all 3 seperated by a comma. default=pdf,jpg,png
+# - --dir: full path to a directory on the current machine you'd like to post to DocuVision API. default=current directory
+# - --loglevel: how verbose you want to logging to be to the docuvision.log file
 # Results:
 # - will save a .json file alongside each item posted to the DocuVision API once results are received
 # - will append logging info to docuvision.log in same dir as this script is run from
+# - will save any pending jobs that are still in progress or have not resulted by the end of the script as (jobid filepath) lines in pendingjobs.docuvision
 
-import pandas as pd
-import numpy as np
-import requests, hashlib, json, os, logging, sys, datetime, argparse
+import requests, hashlib, json, os, logging, sys, datetime, argparse, time
 from pathlib import Path
 from encodings.base64_codec import base64
 
+#get command line parameters passed
+ap = argparse.ArgumentParser("docuvision_api_sample", epilog="For help contact support@hank.ai") #, exit_on_error=False)
+ap.add_argument("--types", help="pdf, png, or jpg. may include up to all 3 seperated by a comma. ", 
+    default="pdf,png,jpg", type=str)
+ap.add_argument("--dir", help="full path to a directory on the current machine to process", 
+    default=".", type=str)
+ap.add_argument("--loglevel", help="Logging level. Options are DEBUG, INFO, WARNING, ERROR, CRITICAL.", 
+    default="DEBUG", type=str)
+
+args, unknown = ap.parse_known_args()
+if args.dir == '.': args.dir=os.path.dirname(os.path.realpath(__file__))
+print("DOCUVISION SCRIPT STARTED")
+print("args = ", args)
+
+#setup logging
+logging.basicConfig(
+    filename='docuvision.log', 
+    filemode='a', 
+    level=args.loglevel.upper(),
+    format='%(asctime)s:%(levelname)s:%(message)s')
+logging.info("STARTING NEW JOB. {}".format(datetime.datetime.now()))
+logging.info("Processing {} filetypes in {} ...".format(args.types, args.dir))
+
+#%% SETUP AUTH
+#make sure token and address are set in env vars (will use the default address here if not)
+APIADDRESS = os.environ.get('DOCUVISION_API_ADDRESS', 
+    "https://services.hank.ai/docuvision/v1/tasks/") #PROD
+    #"https://services-dev.hank.ai/docuvision/v1/tasks/") #DEV
+
+APITOKEN = os.environ.get('DOCUVISION_API_TOKEN', None)
+if APITOKEN is None:
+    logging.error("DOCUVISION_API_TOKEN environment variable is not set. Aborting")
+    sys.exit()
+if APIADDRESS is None:
+    logging.error("DOCUVISION_API_ADDRESS environment variable is not set. Aborting")
+    sys.exit()
+if APIADDRESS[-1] != "/": APIADDRESS+="/"
+logging.info("APIADDRESS loaded. Using {}".format(APIADDRESS))
+logging.info("APITOKEN loaded. Using {}...".format(APITOKEN[:15]))
+
+
 #expects filepath to be a Path() object
 #timeout is in seconds
-def hankai_submit_job(filepath, timeout=10):
+#writes the newly created 'jobid filepath' as a line in pendingjobs.docuvision file in current directory
+#returns job id (int)
+def hankai_submit_job(filepath, timeout=60):
     with open(file, 'rb') as f:
         raw_bytes = f.read()
         enc_bytes = base64.encodebytes(raw_bytes)
     enc_string = enc_bytes.decode("utf-8")
     md5_sum = hashlib.md5(raw_bytes).hexdigest()
-    
+    headers = {"x-api-key": APITOKEN }
     req = {
         "name": "customername_job_x",
         "request": {
-            "securityToken": APITOKEN,
             "document": {
                 "name": filepath.name,
                 "dataType": "blob",
                 "encodingType": "base64/utf-8",
-                "mimeType": filepath.suffix,
+                "mimeType": filepath.suffix[1:], #removes the leading period
                 "model": "base-medrec-anesthesia", #modify this to the model you want to consume
                 "confidenceInterval": 0.7, #modify this to your liking
                 "isPerformOCR": True, #if set your result will have OCR'd words and bounding boxes
@@ -54,97 +94,111 @@ def hankai_submit_job(filepath, timeout=10):
             }
         }
     }
-    
     resp = requests.request(
         method="POST",
-        url=f"{APIADDRESS}docuvision/v1/tasks",
+        url=f"{APIADDRESS}", #docuvision/v1/tasks/7",
+        headers=headers,
         data=json.dumps(req),
         timeout=timeout
     )
     rjson = json.loads(resp.content)
     print(resp.content)
     req_id = rjson.get("id")
-    logging.info(f"Job posted (id={req_id}). Filename={filepath.name}")
+    req_meta = rjson.get("metadata", {})
+    if 'apiKey' in req_meta.keys(): req_meta['apiKey']='hidden'
+    logging.info(f"Job posted. status={resp.status_code}. id={req_id}. file={filepath.name}. respmeta={req_meta}")
 
-#setup logging
-logging.basicConfig(
-    filename='docuvision.log', 
-    filemode='a', 
-    level=logging.DEBUG,
-    format='%(asctime)s:%(levelname)s:%(message)s')
-logging.info("STARTING NEW JOB. {}".format(datetime.datetime.now()))
+    if req_id is not None: 
+        #write the job id of the newly created job to local file 
+        with open('pendingjobs.docuvision', 'a') as f:
+            f.write(f"{req_id} {filepath}\n")
+    return req_id
 
-#make sure token and address are set in env vars
-os.environ['DOCUVISION_API_TOKEN'] = 'hankai_2020'
-os.environ['DOCUVISION_API_ADDRESS'] = "https://ajy0m5kz2m.execute-api.us-east-1.amazonaws.com/prod/"
-APITOKEN = os.environ.get('DOCUVISION_API_TOKEN', None)
-APIADDRESS = os.environ.get('DOCUVISION_API_ADDRESS', None)
-if APITOKEN is None:
-    logging.error("DOCUVISION_API_TOKEN environment variable is not set. Aborting")
-    sys.exit()
-if APIADDRESS is None:
-    logging.error("DOCUVISION_API_ADDRESS environment variable is not set. Aborting")
-    sys.exit()
+#gets the api response for a given jobid
+def hankai_get_results(jobid):
+    logging.debug("Getting {}{}".format(APIADDRESS, jobid))
+    headers = {"x-api-key": APITOKEN }
+    resp = requests.request(
+        method="GET",
+        url=f"{APIADDRESS}{jobid}",
+        headers=headers
+    )
+    return resp
 
-#get command line parameters passed
-ap = argparse.ArgumentParser("docuvision_api_sample", epilog="For help contact support@hank.ai") #, exit_on_error=False)
-ap.add_argument("--types", help="pdf, png, or jpg. may include up to all 3 seperated by a comma. ", 
-    default="pdf,png,jpg", type=str)
-ap.add_argument("--dir", help="full path to a directory on the current machine to process", 
-    default=".", type=str)
-args, unknown = ap.parse_known_args()
-if args.dir == '.': args.dir=os.path.dirname(os.path.realpath(__file__))
-logging.info("Processing {} filetypes in {} ...".format(args.types, args.dir))
+#returns 1 if the job is complete (even if state is error) or 0 if not found or in progress
+def hankai_check_job_complete(resp):
+    try:
+        rjson= json.loads(resp.content)
+        if (rjson.get("state") and rjson.get("state") != "In-Progress"):
+            return 1
+    except: 
+        logging.error(f"in hank_check_job_complete(resp). resp={resp}")
+    return 0
 
-### WIP ###
-def hankai_get_results(jobid):    
-    req_in_prog = True
-    for i in range(max_retries):
-        time.sleep(retry_delay)
-        result = requests.request(
-            method="GET",
-            url=f"{base_url}docuvision/v1/tasks/{req_id}",
-        )
-        result_content = json.loads(result.content)
-        print(f"Attempt {i + 1 }state: {result_content.get('state')}")
-        if (result_content.get("state") and result_content.get("state") != "In-Progress"):
-            break
-    
-    print("\n\nresponse received:\n\n")
-    for key, val in result_content.items():
-        if not key == "request":
-            print(f"{key}: {val}")
+#writes out the results to a .json file of same base filestem+_jobid as original file
+def hankai_write_json_results(jobid, document_filepath, apiresponse):
+    dfP = Path(document_filepath)
+    jsonfp = dfP.parent / (dfP.stem + f'_{jobid}.json')
+    logging.debug(f"Writing api response for jobid={jobid} to {jsonfp}")
+    with open(jsonfp, 'w') as f:
+        jsonapir = json.loads(apiresponse.content)
+        jsonapir['metadata']['apiKey']="{}...".format(jsonapir['metadata']['apiKey'][:15])
+        json.dump(jsonapir, f, indent=2)
 
-#go through each filetype in the directory, recursively (thus rglob), and send files to the api
-dirP = Path(args.dir+'/')
-for type in args.types.split(','):
-    files = list(dirP.rglob(f"*.{type}"))
-    logging.info("Processing {:,} {} ...".format(len(files), type))
-    for file in files:
-        resp = hankai_submit_job(file)
-        #handle polling for results next 
+#%% SUBMIT JOBS
+if 1:
+    #go through each filetype in the directory, recursively (thus rglob), and send files to the api
+    dirP = Path(args.dir+'/')
+    newjobids = []
+    for type in args.types.split(','):
+        files = list(dirP.rglob(f"*.{type}"))
+        logging.info("Processing {:,} {} in {} ...".format(len(files), type, args.dir))
+        for file in files:
+            jobid = hankai_submit_job(file)
+            newjobids.append(jobid)
+        logging.info(f"Done sending {type}s from {args.dir}.")
 
-logging.info("JOB COMPLETED")
+#%% CHECK FOR JOB RESULTS
+
+#if you're testing and want to change the loglevel dynamically use something like this
+# logging.getLogger().setLevel(logging.DEBUG)
+
+pendingjobs = []
+#read in the 'pendingjobs.docuvision' file and get in-progress (jobid filepath) from it
+logging.debug("Reading pending jobs from pendingjobs.docuvision ...")
+with open('pendingjobs.docuvision', 'r') as f:
+    for line in f.readlines():
+        logging.debug(line.strip())
+        jobid, filepath = line.split(" ", 1)
+        pendingjobs.append({'jobid':jobid, 'filepath':filepath.strip()})
+
+MAXRETRIES = 10
+RETRYDELAY = 30
+logging.info("{:,} pending jobs still in progress. Checking on them now ...".format(len(pendingjobs)))
+
+for i in range(1, MAXRETRIES+1):
+    for pj in pendingjobs:
+        time.sleep(1) 
+        logging.debug("Retrieving jobid {}".format(pj['jobid']))
+        res = hankai_get_results(pj['jobid'])
+        complete = hankai_check_job_complete(res)
+        if complete:
+            hankai_write_json_results(pj['jobid'], pj['filepath'], res)
+            logging.info(f"Completed jobid={pj['jobid']}")
+            pendingjobs.remove(pj)
+    if len(pendingjobs)==0:
+        break
+    logging.info("{:,} jobs still in progress.".format(len(pendingjobs)))
+    logging.info(f"Sleeping for {RETRYDELAY} seconds then trying again. Try #{i} of {MAXRETRIES}.")
+    time.sleep(RETRYDELAY)
+
+#overwrite the pending jobs file with the ones still pending
+logging.debug("Writing pending jobs ({}) still in progress to pendingjobs.docuvision ...".format([x['jobid'] for x in pendingjobs]))
+with open('pendingjobs.docuvision', 'w') as f:
+    for pj in pendingjobs:
+        f.write(f"{pj['jobid']} {pj['filepath']}\n")
+
+logging.info("DOCUVISION SCRIPT COMPLETE")
 
 #%%
-def hankai_submit_job(request, maxretries=10):
-    import time
- 
- 
-img_or_pdf_path = "C:/tmp/test-out/BetheaJanieMMRN000017902_pg1.png"
-mime_type = "png"
-base_url = "https://docuvision-api.hank.ai/"
-max_retries, retry_delay  = 60, 5.0 # 60 total retries x 5 sec delay per retry = 5 minutes
- 
-
- 
-resp = requests.request(
-    method="POST",
-    url=f"{base_url}docuvision/v1/tasks/",
-    data=json.dumps(test_req),
-    timeout=10,
-)
-req_content = json.loads(resp.content)
-req_id = req_content.get("id")
-print(f"request id: {req_id}")
 
